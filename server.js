63c78906adfd0166ef1ef58a278cf551ed7fdaa6
harryw123545 <harrywakeling@hotmail.co.uk>
2021-07-27 19:36:59 +0100

    changing port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(3000);
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port);
 
 app.use(express.static('public'));
 
